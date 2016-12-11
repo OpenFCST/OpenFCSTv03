36866132e05d390a5b00a9a0222e8110d7a4ee53
OpenFCST <secanell@ualberta.ca>
2016-12-11 12:01:29 -0700

    bug fix on src/fcst/source/solvers/adaptive_refinement.cc

--- src/fcst/source/solvers/adaptive_refinement.cc
+++ src/fcst/source/solvers/adaptive_refinement.cc
@@ -114,7 +114,7 @@ FuelCell::ApplicationCore::AdaptiveRefinement<dim>::initialize(ParameterHandler&
     {
         this->n_ref = param.get_integer("Number of Refinements");
         app_linear->get_data()->enter("n_ref",this->n_ref);
-        this->data->enter("n_ref",this->n_ref); // for transient solver
+        this->data->enter("n_ref",this->n_ref);
         
         output_initial_mesh = param.get_bool("Output initial mesh");
         filename_initial_mesh = param.get("Output initial mesh filename");
@@ -140,7 +140,6 @@ FuelCell::ApplicationCore::AdaptiveRefinement<dim>::initialize(ParameterHandler&
     app->initialize(param);
     
     this->data->enter_flag("perform_ar",true);
-    this->data->enter_flag("cycle_ran_once",false);
 }
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -211,208 +210,136 @@ FuelCell::ApplicationCore::AdaptiveRefinement<dim>::run_app( std::vector<double>
         
     // --- a copy of the triangulation object to store the information on the coarse mesh ---
     app_linear->store_triangulation(coarse_triangulation);
-    app_linear->initialize_solution(coarse_solution);
-    
-    /**
-     * Variable @simulation_is_not_over is used to control the while loop below. It is only used
-     * in transient simulations and is set to false as soon they reach the final time layer.
-     */
-    bool simulation_is_not_over=true;
+    app_linear->initialize_solution(coarse_solution);    
 
-    /**
-     * Vector used to collect all responses (which are vectors as well). It is sent to the
-     * data object so it can be accessed from the solver. For example, it is accessed from the
-     * @TransientBase class for the responses output at the end of a transient simulation.
-     */
-    std::vector< std::vector<double> > all_responses;
-    
-    /*
-     * This variable is sent to the data object to let the solver know if the vector of responses
-     * @all_responses is ready to be collected. See @ThetaScheme for the usage example.
-     */
-    this->data->enter_flag("responses_ready",false);
-    
-    FEVector u_old;
-    if (this->data->get_transient_solver() != FuelCell::ApplicationCore::TransientSolver::NONE)
-    {
-        u_old.reinit(solution);
-        u_old=solution;
-        app_linear->add_vector_for_transfer(&u_old); 
-    }
-    unsigned int n_ref_copy = this->n_ref;
-    
-    while (simulation_is_not_over)
-    {     
-        // Send n_ref to the data object
-        this->data->enter("n_ref",this->n_ref);        
+    // Send n_ref to the data object
+    this->data->enter("n_ref",this->n_ref);        
         
-        // Variable @cycle_ran_once might be changed by the transient solver. See @ThetaScheme for usage.
-        if (this->data->flag("cycle_ran_once"))
-            this->n_ref=1;
-        else
-            this->n_ref=n_ref_copy;
-            
-        // --- adaptive refinement loop
-        for(unsigned int cycle = 0; cycle < this->n_ref; ++cycle)
-        {           
-            FcstUtilities::log << "Cycle = " << cycle << " : " << std::endl;
-            
-            bool last_cycle = false;
-            this->data->enter_flag("last_cycle",last_cycle);
-            this->data->enter_flag("go_to_next_time_layer",false);
-            
-            std::ostringstream grid_streamOut;
-            std::string        grid_filename;            
-            grid_streamOut << cycle;
-            grid_filename = "fuel_cell_grid_cycle_" + grid_streamOut.str();
-            
-            std::ostringstream sol_streamOut;
-            std::string sol_filename;            
-            sol_streamOut << std::setfill('0') << std::setw(5) << no_data_file;
-            sol_filename = filename_final_sol+"_Cycle_" + grid_streamOut.str() + "_Sol_" + sol_streamOut.str();
-            //sol_filename = "fuel_cell_solution_DataFile_" + sol_streamOut.str() + "_Cycle_" + grid_streamOut.str();
-            
-            app_linear->get_data()->enter("Refinement", cycle);
-                      
-            if (cycle == 0)
-            {
-                // --- initialize initial guess ---
-                app_linear->initialize_solution(solution);
-                if (this->data->get_transient_solver() != FuelCell::ApplicationCore::TransientSolver::NONE)
-                {
-                    u_old=solution;  // u_old is transferred to new meshes with solution, so there is no need to reinitialize it
-                    this->data->enter_flag("go_to_next_time_layer",true);
-                }                
-            }
-            else
-            {
-                // Estimate error per cell for the subsequent adaptive refinement.
-                // The default implementation is supposed to be given in DoFApplicatioin<dim> class.
-                // The custom implementation is usually given in the actual linear application class
-                // using KellyErrorEstimator<dim>::estimate() function.
-                FcstUtilities::log << "Entering estimate..."<<std::endl;
-                app->estimate(vectors);                
-                FcstUtilities::log << "Exited estimate"<<std::endl;
-                
-                if( nonlinear_solver_for_linear_problem )
-                    app_linear->initialize_solution(solution);
-                
-                // Remesh and start with an initial "coarse" solution. Note that
-                // in order to transfer the solution, add_vector_for_transfer needs
-                // to be initialized (see before loop).
-                if (this->data->flag("perform_ar"))
-                    app->remesh();
-                
-                FcstUtilities::log << "Exited remesh"<<std::endl;  
-            }
-            
-            // -- solve the nonlinear system of equations ---
-            try
-            {
-                FcstUtilities::log << "Solving..." << std::endl;
-
-                if( cycle == this->n_ref - 1 ) {
-                    last_cycle = true;
-                    this->data->enter_flag("last_cycle",true);
-                }     
-                
-                if (this->data->get_transient_solver() != FuelCell::ApplicationCore::TransientSolver::NONE)
-                    this->data->enter("old_transient_solution",u_old);
-                app->solve(solution, vectors);
-                
-                FcstUtilities::log << "Solution: " << solution.n_blocks() << ':';
-                for(unsigned int b = 0; b < solution.n_blocks(); ++b)
-                    FcstUtilities::log << ' ' << solution.block(b).size();
-                FcstUtilities::log << std::endl;
-                
-                if(output_intermediate_resp || last_cycle)
-                {
-                    //--- Resize the response vector and copy to it all responses
-                    app->evaluate(vectors);
-                    //Provisional while evaluate() is not changed
-                    app_linear->responses (resp,vectors);
-                    if (this->data->get_transient_solver() != FuelCell::ApplicationCore::TransientSolver::NONE)
-                    {
-                        // Add vector of responses to the end of all_responses
-                        all_responses.push_back(resp);
-                    }
-                    //FcstUtilities::log<< "Current density : "<<-resp[0]<<"A/cm^2"<<std::endl;
-                }
-                
-                if(output_intermediate_sol || (last_cycle && output_final_sol) )
-                {
-                    // Only for debug:
-                    // app_linear->grid_out(grid_filename);
-                    app->data_out(sol_filename,vectors);
-                }
-                
-                if(last_cycle)
-                {
-                    app_linear->transfer_solution_to_coarse_mesh(coarse_triangulation,
-                                                                 coarse_solution,
-                                                                 solution);
-                }
-                
-                
-                if( L1_L2_error_and_convergence_rate )
-                    app_linear->compute_L1_L2_error_and_convergence_rate(solution, cycle, convergence_tables);
-                
-                // -- Update convergence flag:
-                app->get_data()->enter_flag("Newton convergence", true);
-            }
-            catch(const std::exception& e)
-            {
-                app->get_data()->enter_flag("Newton convergence", false);
-                FcstUtilities::log << e.what() << std::endl;
-            }
-            
-            FcstUtilities::log.pop();
-            
-        } // ARM LOOP
+    // --- adaptive refinement loop
+    for(unsigned int cycle = 0; cycle < this->n_ref; ++cycle)
+    {           
+        FcstUtilities::log << "Cycle = " << cycle << " : " << std::endl;
         
-        if( L1_L2_error_and_convergence_rate )
-            print_convergence_table();
+        bool last_cycle = false;
+        this->data->enter_flag("last_cycle",last_cycle);
         
+        std::ostringstream grid_streamOut;
+        std::string        grid_filename;            
+        grid_streamOut << cycle;
+        grid_filename = "fuel_cell_grid_cycle_" + grid_streamOut.str();
         
-        if (gradients == true)
+        std::ostringstream sol_streamOut;
+        std::string sol_filename;
+        sol_streamOut << std::setfill('0') << std::setw(5) << no_data_file;
+        sol_filename = filename_final_sol+"_Cycle_" + grid_streamOut.str() + "_Sol_" + sol_streamOut.str();        
+        
+        app_linear->get_data()->enter("Refinement", cycle);
+                    
+        if (cycle == 0)
         {
-            try {
-                dresp_dl.clear();
-                dresp_dl.resize(app_linear->get_n_resp(), std::vector<double> (app_linear->get_n_dvar()));
-                app_linear->solve_direct(dresp_dl,
-                                         vectors);
-            }
-            catch (const std::exception& e)
-            {
-                FcstUtilities::log << e.what() << std::endl;
-            }
+            // --- initialize initial guess ---
+            app_linear->initialize_solution(solution);             
+        }
+        else
+        {
+            // Estimate error per cell for the subsequent adaptive refinement.
+            // The default implementation is supposed to be given in DoFApplicatioin<dim> class.
+            // The custom implementation is usually given in the actual linear application class
+            // using KellyErrorEstimator<dim>::estimate() function.
+            FcstUtilities::log << "Entering estimate..."<<std::endl;
+            app->estimate(vectors);                
+            FcstUtilities::log << "Exited estimate"<<std::endl;
+            
+            if( nonlinear_solver_for_linear_problem )
+                app_linear->initialize_solution(solution);
+            
+            // Remesh and start with an initial "coarse" solution. Note that
+            // in order to transfer the solution, add_vector_for_transfer needs
+            // to be initialized (see before loop).
+            if (this->data->flag("perform_ar"))
+                app->remesh();
+            
+            FcstUtilities::log << "Exited remesh"<<std::endl;  
         }
         
-        std::vector< std::string > name_resp = app_linear->get_name_responses();
+        // -- solve the nonlinear system of equations ---
+        try
+        {
+            FcstUtilities::log << "Solving..." << std::endl;
+
+            if( cycle == this->n_ref - 1 ) {
+                last_cycle = true;
+                this->data->enter_flag("last_cycle",true);
+            }     
+            
+            app->solve(solution, vectors);
+            
+            FcstUtilities::log << "Solution: " << solution.n_blocks() << ':';
+            for(unsigned int b = 0; b < solution.n_blocks(); ++b)
+                FcstUtilities::log << ' ' << solution.block(b).size();
+            FcstUtilities::log << std::endl;
+            
+            if(output_intermediate_resp || last_cycle)
+            {
+                //--- Resize the response vector and copy to it all responses
+                app->evaluate(vectors);
+                //Provisional while evaluate() is not changed
+                app_linear->responses (resp,vectors);
+                //FcstUtilities::log<< "Current density : "<<-resp[0]<<"A/cm^2"<<std::endl;
+            }
+            
+            if(output_intermediate_sol || (last_cycle && output_final_sol) )
+            {
+                // Only for debug:
+                // app_linear->grid_out(grid_filename);
+                app->data_out(sol_filename,vectors);
+            }
+            
+            if(last_cycle)
+            {
+                app_linear->transfer_solution_to_coarse_mesh(coarse_triangulation,
+                                                                coarse_solution,
+                                                                solution);
+            }
+            
+            
+            if( L1_L2_error_and_convergence_rate )
+                app_linear->compute_L1_L2_error_and_convergence_rate(solution, cycle, convergence_tables);
+            
+            // -- Update convergence flag:
+            app->get_data()->enter_flag("Newton convergence", true);
+        }
+        catch(const std::exception& e)
+        {
+            app->get_data()->enter_flag("Newton convergence", false);
+            FcstUtilities::log << e.what() << std::endl;
+        }
         
-        for (unsigned int i=0; i<resp.size(); ++i)
-            FcstUtilities::log<<"Response "<<name_resp[i].c_str()<<" is : "<<resp[i]<<std::endl;
+        FcstUtilities::log.pop();
         
-        /**
-         * If the model is steady-state, then set @simulation_is_not_over to false to stop the loop that is
-         * designed for the transient problems. Otherwise, receive the value of that flag from the transient solver.
-         */
-        if (this->data->get_transient_solver() == FuelCell::ApplicationCore::TransientSolver::NONE)
-            simulation_is_not_over=false;
-        else
-            simulation_is_not_over=this->data->flag("simulation_is_not_over");        
+    } // ARM LOOP
+    
+    
+    if (gradients == true)
+    {
+        try {
+            dresp_dl.clear();
+            dresp_dl.resize(app_linear->get_n_resp(), std::vector<double> (app_linear->get_n_dvar()));
+            app_linear->solve_direct(dresp_dl,
+                                        vectors);
+        }
+        catch (const std::exception& e)
+        {
+            FcstUtilities::log << e.what() << std::endl;
+        }
+    }
+    
+    std::vector< std::string > name_resp = app_linear->get_name_responses();
+    
+    for (unsigned int i=0; i<resp.size(); ++i)
+        FcstUtilities::log<<"Response "<<name_resp[i].c_str()<<" is : "<<resp[i]<<std::endl;
     
     no_data_file++;
-    } // while simulation_is_not_over
-    
-    // Send all responses to the data object. Used in transient applications only.
-    if (this->data->get_transient_solver() != FuelCell::ApplicationCore::TransientSolver::NONE)
-    {
-        this->data->enter("all_responses",all_responses);
-        this->data->enter_flag("responses_ready",true);
-        // Will not solve anything but will output total responses
-        app->solve(solution, vectors);
-    }
 }
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
